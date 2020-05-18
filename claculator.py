ad31eaecb6694f867cdd335b9e1a5711ace9798d
dvm030 <dvmanjunatha@gmail.com>
2020-05-18 21:49:21 +0530

    my third program calculator

---
+++ claculator.py
@@ -0,0 +1,25 @@
+#  considering two numbers
+
+num1=float(input())
+num2=float(input())
+
+# calculations
+sum= num1+num2
+diff=num1-num2
+prod=num1*num2
+div=num1/num2
+mod=num1//num2
+
+# sum of two numbers
+
+print(sum)
+#print("the sumof two numbers" sum)
+
+# difference of two numbers
+
+print(diff)
+
+print(prod)
+print(div)
+print(mod)
+
