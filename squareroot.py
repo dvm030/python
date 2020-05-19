0c338fcbe58c1c7962ad531f215c519af2e6dc9c
dvm030 <dvmanjunatha@gmail.com>
2020-05-19 21:19:48 +0530

    square root python code

---
+++ squareroot.py
@@ -0,0 +1,16 @@
+# Program to find the square roort of a given number
+
+num=9
+
+num_sqrt= num**0.5
+
+# display the square root of given number
+
+print("The square root of %0.3f is %0.3f" %(num, num_sqrt))
+
+
+
+
+
+
+
