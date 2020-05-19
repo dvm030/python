a7c4f0d120234faf621bb6fe019e898653fac06e
dvm030 <dvmanjunatha@gmail.com>
2020-05-19 21:26:04 +0530

    finding squareroot of entered number

---
+++ msquare.py
@@ -0,0 +1,10 @@
+# modified square root code
+# written by Dr. D.V. Manjunatha
+
+num= float(input("Enter the  number >>"))
+
+num_sqrt= num**0.5
+
+print(" The square root of %0.2f is %0.2f" %(num, num_sqrt))
+
+
