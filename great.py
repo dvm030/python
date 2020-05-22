f03570b1005589518e78eaa60e201fd91f0b5b09
dvm030 <dvmanjunatha@gmail.com>
2020-05-22 22:19:31 +0530

    the greates of two numbers code

---
+++ great.py
@@ -0,0 +1,11 @@
+a= int(input("Enter the value of first number:"))
+b=int(input("Enter the value of Second Number:"))
+
+if(a>b):
+
+	print("{0} is greater than {1}".format(a,b))
+
+else:
+	
+	print("{0} is greater than {1}".format(b,a))
+
